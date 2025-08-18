9a610cb83b8f94723dd5f20cdaf9d2ca317dfab8
ab-dx <abhinavdatta1@gmail.com>
2025-08-18 14:40:07 +0530

    [add]: basic home page elements

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import { AuthUserProvider } from "@/context/AuthUserContext";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <AuthUserProvider>
+          <Navbar />
           {children}
         </AuthUserProvider>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import styles from "./Home.module.css"
+import { ArrowDown } from "lucide-react";
+
 export default function Home() {
   return (
-    <>
-      Celesta '25
-    </>
+    <div className={`flex flex-col justify-center items-center ${styles.background}`}>
+      <section className="flex h-[90vh] justify-center items-center">
+        <Image src="/images/banner-logo.png" alt="banner-logo" width={1000} height={500} />
+      </section>
+      <ArrowDown className="text-white animate-bounce" />
+      <section className="flex my-20 justify-center items-center">
+        <h1 className="text-white font-bold text-6xl state-wide">Past Performers</h1>
+      </section>
+    </div>
   );
 }
