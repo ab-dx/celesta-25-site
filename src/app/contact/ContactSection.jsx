cadb2ab6cee4dfe5154cd0f1773803d7466257bd
rajprashant3302 <rajprashant3302@gmail.com>
2025-09-06 23:09:10 +0530

    UI of register/login/contact page changed

--- src/app/contact/ContactSection.jsx
+++ src/app/contact/ContactSection.jsx
@@ -1,58 +1,75 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
 import { FaInstagram, FaLinkedin, FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 const ContactSection = () => {
-    return (
-        <div className=''>
-            <motion.div
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.5, delay: 0.3 }}
-                className="flex flex-col justify-center gap-10 text-xl  border-2 border-white rounded-lg p-6 w-full lg:w-[30vw] md:h-[60vh] lg:h-[75vh] h-full"
-            >
-                <div className="flex items-center gap-4">
-                    <FaEnvelope className="tex-white text-2xl" />
-                    <p>celesta@iitp.ac.in</p>
-                </div>
-                <div className="flex items-center gap-4">
-                    <FaPhoneAlt className="text-white text-2xl" />
-                    <div>
-                        <p>Abhitesh Shukla</p>
-                        <p>+91 78977 21774</p>
-                    </div>
-                </div>
-                <div className="flex items-center gap-4">
-                    <FaPhoneAlt className="text-white text-2xl" />
-                    <div>
-                        <p>Ananta Nanda</p>
-                        <p>+91 73258 73426</p>
-                    </div>
-                </div>
-                <div className="flex items-center gap-4">
-                    <FaMapMarkerAlt className="text-white text-2xl" />
-                    <div>
-                        <p>Celesta Office, IIT Patna</p>
-                        <p>Bihta, Patna</p>
-                    </div>
-                </div>
-
-                <div className="mt-6 ">
-                    <h3 className="text-xl font-medium text-white mb-4">Follow Us on social Media</h3>
-                    <div className="flex gap-6 text-3xl">
-                        <a href="https://www.instagram.com/celestaiitp_official?utm_source=ig_web_button_share_sheet&igsh=MXQ3b3RrdGdteWVkOA==" target="_blank" rel="noreferrer">
-                            <FaInstagram className="hover:text-pink-400 transition-colors" />
-                        </a>
-                        <a href="https://www.linkedin.com/company/celesta-iit-patna/" target="_blank" rel="noreferrer">
-                            <FaLinkedin className="hover:text-blue-400 transition-colors" />
-                        </a>
-                    </div>
-                </div>
-            </motion.div>
+  return (
+    <div className="w-full max-w-lg lg:h-[73vh]  lg:w-[25vw] ">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="flex flex-col gap-6 w-full  py-10 px-10 md:px-6 lg:py-18 md:py-10 bg-white/10 backdrop-blur-xl rounded-2xl border border-white/20  shadow-lg overflow-hidden"
+      >
+        {/* Email */}
+        <div className="flex items-center gap-4">
+          <FaEnvelope className="text-white text-3xl" />
+          <p className="text-white text-xl">celesta@iitp.ac.in</p>
         </div>
-    )
-}
 
-export default ContactSection
+        {/* Contacts */}
+        <div className="flex items-center gap-4">
+          <FaPhoneAlt className="text-white text-3xl" />
+          <div className="text-white ">
+            <p className="text-xl">Abhitesh Shukla</p>
+            <p>+91 78977 21774</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <FaPhoneAlt className="text-white text-3xl" />
+          <div className="text-white ">
+            <p className="text-xl">Ananta Nanda</p>
+            <p>+91 73258 73426</p>
+          </div>
+        </div>
+
+        {/* Location */}
+        <div className="flex items-center gap-4">
+          <FaMapMarkerAlt className="text-white text-3xl" />
+          <div className="text-white ">
+            <p className="text-xl">Celesta Office, IIT Patna</p>
+            <p>Bihta, Patna</p>
+          </div>
+        </div>
+
+        {/* Social Media */}
+        <div className="mt-6">
+          <h3 className="text-2xl font-medium text-white mb-4">Follow Us on Social Media</h3>
+          <div className="flex gap-6 text-3xl">
+            <a
+              href="https://www.instagram.com/celestaiitp_official/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-pink-400 transition-colors"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/celesta-iit-patna/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-blue-400 transition-colors"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default ContactSection;
