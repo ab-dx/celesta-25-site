038dfa4436a48ac30fb38454db833f6661b27df9
Pradeep Sagitra <sagitrapradeep2006@gmail.com>
2025-09-27 14:48:44 +0530

    new Card has been created for events page

--- src/app/events/event-card.js
+++ src/app/events/event-card.js
@@ -2,13 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function EventCard({ name, img_src, register_link }) {
-  return <>
-    <div className="p-4 rounded-xl w-[28rem] transition hover:scale-105 shadow-xl">
-      <Image src={img_src} alt="img" className="rounded-xl h-[32rem]" width={500} height={200} />
-      <div className="translate-y-[-10rem] gap-4 m-6">
-        <h1 className="text-white uppercase text-4xl font-extrabold">{name}</h1>
-        <Link href={register_link}><button className="px-2 py-1 mt-4 rounded-md border-2 border-white hover:bg-white hover:text-gray-800 transition uppercase tracking-[2px] text-md">Register</button></Link>
+  return (
+    <>
+      <div className="mx-auto border-2 border-white rounded-4xl bg-gradient-to-br from-gray-800 blue-950 via-black to-green-800 w-100 hover:scale-105">
+        <div className="border-2 border-white mx-3 mt-3 rounded-t-4xl flex justify-center border-b-0">
+          <h1 className="text-white font-extrabold uppercase">{name}</h1>
+        </div>
+        <div className="border-2 border-white mx-3 mt-0 overflow-hidden">
+          <Image
+            src={img_src}
+            alt="img"
+            className="transform transition-transform hover:scale-110 focus:scale-110"
+            width={500}
+            height={200}
+          />
+        </div>
+        <div className="flex justify-end">
+          <div className="border-2 border-white m-3 rounded-br-4xl rounded-tl-4xl hover:bg-gradient-to-br from-green-800 to-black w-45 flex justify-center transition-colors">
+            <Link href={register_link} className="text-white hover:cursor-pointer uppercase">
+              Register
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
